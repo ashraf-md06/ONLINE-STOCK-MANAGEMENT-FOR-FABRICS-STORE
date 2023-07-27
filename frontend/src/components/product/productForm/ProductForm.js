40580e8709508f639a0c7cdedde6356bc1d5d80e
MUHAMMEDASHRAF-S <ashrafmd1106@gmail.com>
2023-07-27 12:03:46 +0530

    stock management

---
+++ frontend/src/components/product/productForm/ProductForm.js
@@ -0,0 +1,144 @@
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import Card from "../../card/Card";
+
+import "./ProductForm.scss";
+
+const ProductForm = ({
+  product,
+  productImage,
+  imagePreview,
+  description,
+  setDescription,
+  handleInputChange,
+  handleImageChange,
+  saveProduct,
+}) => {
+  return (
+    <div className="add-product">
+      <Card cardClass={"card"}>
+        <form onSubmit={saveProduct}>
+          <Card cardClass={"group"}>
+            <label>Product Image</label>
+            <code className="--color-dark">
+              Supported Formats: jpg, jpeg, png
+            </code>
+            <input
+              type="file"
+              name="image"
+              onChange={(e) => handleImageChange(e)}
+            />
+
+            {imagePreview != null ? (
+              <div className="image-preview">
+                <img src={imagePreview} alt="product" />
+              </div>
+            ) : (
+              <p>No image set for this poduct.</p>
+            )}
+          </Card>
+          <label>Product Name:</label>
+          <input
+            type="text"
+            placeholder="Product name"
+            name="name"
+            value={product?.name}
+            onChange={handleInputChange}
+          />
+
+          <label>Product Color:</label>
+          <input
+            type="text"
+            placeholder="Product Color"
+            name="category"
+            value={product?.category}
+            onChange={handleInputChange}
+          />
+
+          <label>Stock Inward:</label>
+          <input
+            type="text"
+            placeholder="Stock Inward Quantity"
+            name="price"
+            value={product?.price}
+            onChange={handleInputChange}
+         
+         />
+
+          <label>Product Processing at:</label>
+          <ReactQuill
+            theme="snow"
+            value={description}
+            onChange={setDescription}
+            modules={ProductForm.modules}
+            formats={ProductForm.formats}
+          />
+
+          <label>Stock Outward:</label>
+          <input
+            type="text"
+            placeholder="Stock Outward Quantity "
+            name="quantity"
+            value={product?.quantity}
+            onChange={handleInputChange}
+          />
+
+          {/* <label>Product Description:</label>
+          <ReactQuill
+            theme="snow"
+            value={description}
+            onChange={setDescription}
+            modules={ProductForm.modules}
+            formats={ProductForm.formats}
+          /> */}
+
+          <div className="--my">
+            <button type="submit" className="--btn --btn-primary">
+              Save Product
+            </button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+ProductForm.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ align: [] }],
+    [{ color: [] }, { background: [] }],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["clean"],
+  ],
+};
+ProductForm.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "color",
+  "background",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "video",
+  "image",
+  "code-block",
+  "align",
+];
+
+export default ProductForm;
